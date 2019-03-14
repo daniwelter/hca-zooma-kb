a75d4a11aa0c51549d91b48c8d350734b8c96454
Dani Welter <dwelter@ebi.ac.uk>
2019-03-14 16:25:51 +0000

    Further updates and improvements

--- hca-mappings-extractor/mappings-extractor.py
+++ hca-mappings-extractor/mappings-extractor.py
@@ -9,25 +9,25 @@ INGEST_API_URL = 'http://api.ingest.data.humancellatlas.org/submissionEnvelopes/
 
 pp = pprint.PrettyPrinter(indent=4)
 
-def extract_mappings(envelope_id):
+def extract_mappings(envelope_id, file):
     jsonRaw = requests.get(INGEST_API_URL + envelope_id).json()
 
     biomaterials_link = jsonRaw['_links']['biomaterials']['href']
-    process_json(biomaterials_link, 'biomaterials')
+    process_json(biomaterials_link, 'biomaterials', file)
 
     protocols_link = jsonRaw['_links']['protocols']['href']
-    process_json(protocols_link, 'protocols')
+    process_json(protocols_link, 'protocols', file)
 
     # projects_link = jsonRaw['_links']['projects']['href']
 
 
-def process_json(link, type):
+def process_json(link, type, file):
     done = False
     while not done:
         entries = requests.get(link).json()
 
         for entry in entries['_embedded'][type]:
-            read_properties(entry['content'])
+            read_properties(entry['content'], file)
 
         if 'next' in entries['_links']:
             link = entries['_links']['next']['href']
@@ -36,25 +36,47 @@ def process_json(link, type):
 
 
 
-def read_properties(data):
+def read_properties(data, file, root=None):
     for k, v in data.items():
         if isinstance(v, dict):
             if "ontology" in v:
-                ontology = v['ontology']
-                text = v['text']
+                ontology = v['ontology'].strip()
+                text = v['text'].strip()
 
-                if text in all_mappings:
-                    if ontology not in  all_mappings[text]:
-                        all_mappings[text].append(ontology)
-                else:
-                    all_mappings[text] = [ontology]
+                print(k + "\t" + text + "\t" + ontology)
+                file.write(k + "\t" + text + "\t" + ontology + "\n")
+
+                # entry = text + " - " + ontology
+
+                # if text in all_mappings:
+                #     if ontology not in  all_mappings[text]:
+                #         all_mappings[text].append(ontology)
+                # else:
+                #     all_mappings[text] = [ontology]
+
+                # if root is None:
+                #     root = k
+                # if root in all_mappings:
+                #     if entry not in all_mappings[root]:
+                #         all_mappings[root].append(entry)
+                # else:
+                #     all_mappings[root] = [entry]
+
             else:
-                read_properties(v)
+                read_properties(v, file, k)
 
         elif isinstance(v, list):
             for index, e in enumerate(v):
                 if isinstance(e, dict):
-                    read_properties(e)
+                    if "ontology" in e.keys():
+                        ontology = e['ontology'].strip()
+                        text = e['text'].strip()
+
+                        print(k + "\t" + text + "\t" + ontology)
+                        file.write(k + "\t" + text + "\t" + ontology + "\n")
+
+                    else:
+                        read_properties(e, file, k)
 
 
 if __name__ == '__main__':
@@ -83,17 +105,19 @@ if __name__ == '__main__':
                 '5c054a529460a300074f5007',
                 '5be1bede9460a300074d1fe2',
                 '5c055aca9460a300074f87da',
-                # '5c0ac7f89460a3000750ce46', Tabula muris - broken!
                 '5bfe4d3a9460a300074eebc8',
                 '5c06c6ad9460a300074fc27f',
                 '5c06a9cf9460a300074fc183',
                 '5c06ccdc9460a300074fc2a7',
-                '5c06c34f9460a300074fc246'
+                '5c06c34f9460a300074fc246',
+                '5c51805d5fc0000007998f72'
                     ]
-
+    file = open("all_mappings.text", "w")
     for eid in envelope_ids:
         print("Processing " + eid)
-        extract_mappings(eid)
+        extract_mappings(eid, file)
 
-    pp.pprint(all_mappings)
+    file.close()
+
+    # pp.pprint(all_mappings)
 
